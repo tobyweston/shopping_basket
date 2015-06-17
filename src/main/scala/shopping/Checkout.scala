6b80d3903cd336d701b2e5c501d0d0d5cc2aec9f
Toby Weston <toby.weston@gmail.com>
2015-06-17 20:41:21 +0100

    improvements: simplify the checkout

--- src/main/scala/shopping/Checkout.scala
+++ src/main/scala/shopping/Checkout.scala
@@ -3,7 +3,7 @@ package shopping
 object Checkout {
 
   def price(items: Seq[Fruit]): Int = {
-    val discounts = items.groupBy(identity).map({ case (fruit, fruits) => fruit.discountFor(fruits) })
+    val discounts = items.distinct.map(_.discountFor(items))
     items.map(_.price).sum - discounts.sum
   }
 }
